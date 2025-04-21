9638511e9470bfa1fdde4c9c51ccc41369d49d8b
AndreyBurkov <burkov_andrey_alex@mail.ru>
2025-04-21 08:47:56 +0300

    Validation for game properties was added

--- src/main/java/com/gamefactory/minesweeper/config/GameProperties.java
+++ src/main/java/com/gamefactory/minesweeper/config/GameProperties.java
@@ -1,5 +1,7 @@
 package com.gamefactory.minesweeper.config;
 
+import com.gamefactory.minesweeper.utils.GameConstants;
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -11,5 +13,21 @@ public class GameProperties {
     private int maxGameDurationMinutes;
     private int maxFieldHeight;
     private int maxFieldWidth;
+
+    @PostConstruct
+    private void validateAndUpdateProperties() {
+        if (maxGamesCount < 1) {
+            maxGamesCount = GameConstants.DEFAULT_MAX_GAMES_COUNT;
+        }
+        if (maxGameDurationMinutes < 1) {
+            maxGameDurationMinutes = GameConstants.DEFAULT_MAX_GAME_DURATION_MINUTES;
+        }
+        if (maxFieldHeight < 2 || maxFieldHeight > 30) {
+            maxFieldHeight = GameConstants.DEFAULT_MAX_GAME_HEIGHT;
+        }
+        if (maxFieldWidth < 2 || maxFieldWidth > 30) {
+            maxFieldWidth = GameConstants.DEFAULT_MAX_GAME_WIDTH;
+        }
+    }
 
 }
--- src/main/java/com/gamefactory/minesweeper/utils/GameConstants.java
+++ src/main/java/com/gamefactory/minesweeper/utils/GameConstants.java
@@ -2,6 +2,10 @@ package com.gamefactory.minesweeper.utils;
 
 public interface GameConstants {
 
+    int DEFAULT_MAX_GAMES_COUNT = 1000;
+    int DEFAULT_MAX_GAME_DURATION_MINUTES = 30;
+    int DEFAULT_MAX_GAME_HEIGHT = 30;
+    int DEFAULT_MAX_GAME_WIDTH = 30;
     int MIN_FIELD_HEIGHT = 2;
     int MIN_FIELD_WIDTH = 2;
     char INITIAL_MINE_CHAR = ' ';
